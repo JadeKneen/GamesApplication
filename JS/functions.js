6a125f06867c6ad7db54280926afde2aa09e0279
Jade Kneen <48185660+JadeKneen@users.noreply.github.com>
2020-09-17 12:24:56 +0100

    alphabet array

--- JS/functions.js
+++ JS/functions.js
@@ -23,6 +23,7 @@ function Reset(){
 var MysteryWord = document.getElementById("mysteryWord")
 var Letters = []
 var guessLetters = []
+var Alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z"]
 
 function InsertWord(){
     Letters.push(MysteryWord.value.split(""))
